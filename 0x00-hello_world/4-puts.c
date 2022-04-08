9274eb42d590e9fb9cbff3c95f8f68459c315b14
black-addis <ambayeblack25225@gmail.com>
2022-04-07 18:24:15 -0700

    ugh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * Description:main -Prints out a string to stdout.
- * Return:0 if Success
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if Success.
  */
 int main(void)
 {
