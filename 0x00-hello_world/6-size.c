6df73f5c1f54f18bee176c88eca75cdd19979dee
black-addis <ambayeblack25225@gmail.com>
2022-04-07 19:22:47 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *   main - Entry point
+ *
+ *   Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+       	printf("Size of an init: %d byte(s)\n", sizeof(init));
+	Printf("Size of a long init: % d byte(s)\n", sizeof(long init));
+	printf("Size of a long long init: %d byte(s)\n", sizeof(long long init));
+	Printf("Size of a float: %d byte(s)\n", sizeof(float));	
+	return (0);
+
+}
