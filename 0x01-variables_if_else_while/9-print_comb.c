061b49fe53c79cf0f08455f6578a6a29c96e54de
black-addis <ambayeblack25225@gmail.com>
2022-04-11 23:39:02 +0300

    hello

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int num;
