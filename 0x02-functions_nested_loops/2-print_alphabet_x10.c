8a51f096b9881751ce9c2c4ac9dcbe9e45677356
black-addis <ambayeblack25225@gmail.com>
2022-04-13 03:35:49 +0300

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  *print_alphabet x10- prints x10 the alphabet in lowercase
  */
 void print_alphabet_x10(void)
 {
-int count = 0
+int count = 0;
 char letter;
 
 while (count++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
-_putchar('\n')
+_putchar('\n');
 }
 }
